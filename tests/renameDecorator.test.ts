1151756954801eadc611fecd8bb736af9c3f91ab
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-01-19 23:23:46 +0200

    Added test for rename decorator

--- tests/renameDecorator.test.ts
+++ tests/renameDecorator.test.ts
@@ -23,4 +23,25 @@ describe('renameDecorator', () => {
             preExpr: [],
         });
     });
+    it('should rename class method with custom specified name', async () => {
+        class DummyClass {
+            @rename("I'm custom name!!!")
+            testFn(a: string, b: string) {
+                return { c: a + b };
+            }
+        }
+
+        const dummyInstance = new DummyClass();
+
+        const composed = compose(dummyInstance.testFn, 'hello', 'world');
+
+        expect(JSON.parse(composed.getJson()[0])).toStrictEqual({
+            name: "I'm custom name!!!",
+            args: {
+                a: 'hello',
+                b: 'world',
+            },
+            preExpr: [],
+        });
+    });
 });
